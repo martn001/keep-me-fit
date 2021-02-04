70a1da79072a763d51fbb6329cf6e13c9789e4d4
Ghoster524 <60692171+Ghoster524@users.noreply.github.com>
2021-02-04 21:49:59 +0100

    Created quotes until I thought, why?

---
+++ src/data/Quotes.js
@@ -0,0 +1,14 @@
+// Heeft dit eigenlijk wel nut?
+export default [
+  `Lift heavy shit until<br/> it's not heavy anymore`,
+  'Get fit for life <br/> not just for summer',
+  'What hurts today <br/>makes you stronger tomorrow.',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+  '',
+];
